0570d180df941c440dfcee9a99076958483b6a74
Ariel Alvarez <arialvarez2019@gmail.com>
2023-06-05 13:15:33 -0300

    ejercicio clase 12

---
+++ algoritmo.js
@@ -0,0 +1,9 @@
+let letraInicialPais = "a";
+let chatMsjReunion = "hola";
+let pesoBotellaLitro ="1";
+let estadoServicio = true;
+let nombreCentroFormacion ="cfp27";
+let contraseniaYoutube = '1!._.-,asf_falxse';
+let numeroCelular = "1159129432";
+let nombrePostre = "flan";
+let claveProducto ="123N-976G-54F";
